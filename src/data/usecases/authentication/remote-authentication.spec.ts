f837f857bcd5309b04943596a6e86c38e58ff276
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-06-05 20:31:33 -0300

    chore: add faker for mocked data tests

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,12 @@
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "../../test/mock-http-client"
+import { faker } from "@faker-js/faker"
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
-const makeSut = (url: string = ""): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   // sut: system under test
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
