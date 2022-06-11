fff1f9456b54e9b2120cf464d1a2214ca54be4e8
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-06-11 13:27:14 -0300

    test: ensure Login starts with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,5 +9,11 @@ describe("Login Component", () => {
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId("submit") as HTMLButtonElement // this cast allow get button props and not getByTestId return
     expect(submitButton.disabled).toBe(true)
+
+    const emailStatus = getByTestId("email-status")
+    expect(emailStatus.title).toBe("Campo obrigatório")
+
+    const passwordStatus = getByTestId("password-status")
+    expect(passwordStatus.title).toBe("Campo obrigatório")
   })
 })
