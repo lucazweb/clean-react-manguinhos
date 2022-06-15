7f7cef02f883132e7fbc7ca9396cc20375c6c441
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-06-15 19:12:25 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -59,6 +59,18 @@ describe("Login Component", () => {
     expect(passwordStatus.textContent).toBe(validationStub.errorMessage)
   })
 
+  test("Should show valid email state if validation succeeds ", () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId("email")
+    fireEvent.input(emailInput, {
+      target: { value: faker.internet.password() },
+    })
+    const emailStatus = sut.getByTestId("email-status")
+    expect(emailStatus.title).toBe("Tudo certo!")
+    expect(emailStatus.textContent).toBe("Tudo certo!")
+  })
+
   test("Should show valid password state if validation succeeds ", () => {
     const { sut, validationStub } = makeSut()
     validationStub.errorMessage = null
