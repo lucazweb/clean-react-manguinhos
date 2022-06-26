41e0f896e13499cd1d8b4f89a7e4f8ff30d21ea3
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-06-26 18:25:45 -0300

    test: ensure ValidationBuilder has email helper

--- src/validation/builder/validation-builder.ts
+++ src/validation/builder/validation-builder.ts
@@ -1,5 +1,6 @@
 import { FieldValidation } from "@/validation/protocols/field-validation"
 import { RequiredFieldValidation } from "@/validation/validators/required-filed/required-field-validation"
+import { EmailValidation } from "../validators"
 
 export class ValidationBuilder {
   private constructor(
@@ -13,6 +14,11 @@ export class ValidationBuilder {
 
   required(): ValidationBuilder {
     this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  email(): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName))
     return this
   }
 
