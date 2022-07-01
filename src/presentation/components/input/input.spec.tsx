6ab0eaeb9729f532b237faf99e46668eaab6600c
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-07-01 09:55:54 -0300

    test: add input render test

---
+++ src/presentation/components/input/input.spec.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { render, RenderResult } from "@testing-library/react"
+import { Input } from "./input"
+import Context from "@/presentation/contexts/form/form-context"
+
+const makeSut = (): RenderResult => {
+  return render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name="field" />
+    </Context.Provider>
+  )
+}
+
+describe("Input Component", () => {
+  test("Should render Input component ", () => {
+    const sut = makeSut()
+    const input = sut.getByTestId("field") as HTMLInputElement
+    expect(input).toBeTruthy()
+  })
+})
