90cdc6354646ac01c99e02d5ba0225e43cf2861e
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-07-06 21:11:09 -0300

    test: ensure LocalSaveAccessToken throws if SetStorage throws

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -24,4 +24,13 @@ describe('LocalSaveAccessToken', () => {
     expect(setStorageMock.key).toBe('accessToken')
     expect(setStorageMock.value).toBe(accessToken)
   })
+
+  test('Should throw if SetStorage throws', async () => {
+    const { sut, setStorageMock } = makeSut()
+    jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.save(faker.database.mongodbObjectId())
+    await expect(promise).rejects.toThrow(new Error())
+  })
 })
